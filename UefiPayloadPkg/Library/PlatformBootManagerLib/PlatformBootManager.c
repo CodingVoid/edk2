595568c2fdb48bedf5ef3b8944fa615d317fddd7
Tim Crawford <tcrawford@system76.com>
2020-02-03 10:51:54 -0700

    UefiPayloadPkg: Show boot message as progress text
    Moves 'Press ESC' prompt to bottom of screen
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Change-Id: I0493318e40d0a753dd804dcf3f3ea968adfc8628

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -264,18 +264,22 @@ PlatformBootManagerAfterConsole (
   PlatformRegisterFvBootOption (PcdGetPtr (PcdShellFile), L"UEFI Shell", LOAD_OPTION_ACTIVE);
 
   if (FixedPcdGetBool (PcdBootManagerEscape)) {
-    Print (
-      L"\n"
-      L"    Esc or Down      to enter Boot Manager Menu.\n"
-      L"    ENTER            to boot directly.\n"
-      L"\n"
+    BootLogoUpdateProgress (
+      White,
+      Black,
+      L"Press ESC for Boot Options/Settings",
+      White,
+      0,
+      0
       );
   } else {
-    Print (
-      L"\n"
-      L"    F2 or Down      to enter Boot Manager Menu.\n"
-      L"    ENTER           to boot directly.\n"
-      L"\n"
+    BootLogoUpdateProgress (
+      White,
+      Black,
+      L"Press F2 or Down for Boot Options/Settings",
+      White,
+      0,
+      0
       );
   }
 
