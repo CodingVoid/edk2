6aa976c66cc41f94f56592409fc3dc270aa47620
Matt DeVillier <matt.devillier@gmail.com>
2020-11-24 13:57:46 -0600

    BmBoot: prioritize internal boot devices over external
    This prevents some internally-attached USB devices
    (card readers, etc) from defaulting ahead of SATA/NVMe/eMMC drives.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
+++ MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
@@ -2167,10 +2167,10 @@ BmMatchPartitionDevicePathNode (
 
 /**
   Emuerate all possible bootable medias in the following order:
-  1. Removable BlockIo            - The boot option only points to the removable media
-                                    device, like USB key, DVD, Floppy etc.
-  2. Fixed BlockIo                - The boot option only points to a Fixed blockIo device,
+  1. Fixed BlockIo                - The boot option only points to a Fixed blockIo device,
                                     like HardDisk.
+  2. Removable BlockIo            - The boot option only points to the removable media
+                                    device, like USB key, DVD, Floppy etc.
   3. Non-BlockIo SimpleFileSystem - The boot option points to a device supporting
                                     SimpleFileSystem Protocol, but not supporting BlockIo
                                     protocol.
@@ -2204,7 +2204,7 @@ BmEnumerateBootOptions (
   EmmcCount        = 0;
 
   //
-  // Parse removable block io followed by fixed block io
+  // Parse fixed block io followed by removable block io
   //
   gBS->LocateHandleBuffer (
          ByProtocol,
@@ -2233,9 +2233,9 @@ BmEnumerateBootOptions (
       }
 
       //
-      // Skip the fixed block io then the removable block io
+      // Skip the removable block io then the fixed block io
       //
-      if (BlkIo->Media->RemovableMedia == ((Removable == 0) ? FALSE : TRUE)) {
+      if (BlkIo->Media->RemovableMedia == (Removable == 0)) {
         continue;
       }
 
