bf3c7a515788507bb6397d3b2b9c02543b07ee0e
Matt DeVillier <matt.devillier@gmail.com>
2022-07-15 10:08:19 -0500

    Revert "UefiPayloadPkg: Use Boot Splash"
    This reverts commit 98d1cef9fb0a8dec0724f8f1c2a471dece4bee00.

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -252,9 +252,6 @@ PlatformBootManagerAfterConsole (
   Black.Blue = Black.Green = Black.Red = Black.Reserved = 0;
   White.Blue = White.Green = White.Red = White.Reserved = 0xFF;
 
-  gST->ConOut->ClearScreen (gST->ConOut);
-  BootLogoEnableLogo ();
-
   EfiBootManagerConnectAll ();
   EfiBootManagerRefreshAllBootOption ();
 
